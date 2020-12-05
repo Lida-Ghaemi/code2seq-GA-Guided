a8f134b563b0a1f7f641fd079238497458cbf868
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-12-05 17:48:39 +0330

    Update code2seq.py

--- code2seq.py
+++ code2seq.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     print('Created model')
     if config.TRAIN_PATH:
         a43=[1]
-        model.train(a43)
+        model.train()
     if config.TEST_PATH and not args.data_path:
         results, precision, recall, f1, rouge = model.evaluate()
         print('Accuracy: ' + str(results))
